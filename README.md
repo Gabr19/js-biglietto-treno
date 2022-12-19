0c59698b7d3473a3d051154d6f3b5fc5f458c1b0
Gabr19 <sannagabriele19@gmail.com>
2022-12-19 16:16:38 +0100

    completato if else

--- README.md
+++ README.md
@@ -13,3 +13,12 @@ va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
 
+1)Quanti km devi fare?
+2)Quanti anni hai?
+3)if età=< 64 && età>=19
+    P=km*0,21 euro
+4)else if età>=65
+    P=(km*0,21) - 40%)    
+5)else if età<=18
+    P=(km*0,21) - 20%)    
+
---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+const km = prompt ("Quanti chilometri devi percorrere?")
+    console.log("La distanza da percorrere è: ", km);
+
+const età = prompt ("Quanti anni hai?")
+    console.log("L'età del passeggero è: ", età );
+
+if (età>=19 && età<=64) {
+    let x = km;
+    let y = 0.21;
+    let z = x * y;
+    console.log(z);
+}
+
+else if (età>=65) {
+    let x = km;
+    let y = 0.21;
+    let z = x * y * 0.6;
+    console.log(z);
+}
+
+else {
+    let x = km;
+    let y = 0.21;
+    let z = x * y * 0.8;
+    console.log(z);
+}
